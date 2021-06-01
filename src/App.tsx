18c9358b81d45a442be84bbabf84755baeaebc92
Caitlin <caitlin.sigrist@gmail.com>
2021-06-01 20:53:23 +0200

    wip: react setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/navBar';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './theme';
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
+      <Router>
+      <NavBar />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,8 +25,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      </Router>
     </div>
+    </ThemeProvider>
   );
 }
 
