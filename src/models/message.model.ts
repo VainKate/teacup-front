45e74abfda2075bb8852ee32aaac65863221f7d4
AngelZatch <angelzatch@gmail.com>
2021-06-02 19:02:06 +0200

    feat: send and recieve messages

--- src/models/message.model.ts
+++
@@ -1,9 +0,0 @@
-import { Channel } from './channel.model';
-import { AuthenticatedUser } from './user.model';
-
-export type Message = {
-  id: string;
-  user: Pick<AuthenticatedUser, 'id' | 'nickname'>;
-  channel: Pick<Channel, 'id'>;
-  content: string;
-};
--- src/screens/Channel.tsx
+++ src/screens/Channel.tsx
@@ -3,11 +3,40 @@ import { useParams } from 'react-router';
 import { AuthContext } from '../context/auth';
 import { Channel } from '../models/channel.model';
 import { io, Socket } from 'socket.io-client';
-import { Message } from '../models/message.model';
+import ChatInput from '../components/ChatInput';
+import { Box, createStyles, makeStyles } from '@material-ui/core';
+import { AuthenticatedUser } from '../models/user.model';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    messageList: {
+      height: '100%',
+    },
+    input: {
+      position: 'fixed',
+      bottom: 0,
+      width: '100%',
+    },
+  }),
+);
+
+type SocketAuthPacket = {
+  channel: Pick<Channel, 'id'>;
+  user: Pick<AuthenticatedUser, 'id' | 'nickname'>;
+};
+
+type Message = {
+  id?: string;
+  content: string;
+} & SocketAuthPacket;
 
 const ChannelScreen: React.FC = () => {
   const { user } = useContext(AuthContext);
   const { channelId } = useParams<{ channelId: string }>();
+  const classes = useStyles();
 
   const socket: Socket = io('localhost:8000', {
     transports: ['websocket'],
@@ -22,12 +51,15 @@ const ChannelScreen: React.FC = () => {
     socket
       .on('connect', () => {
         socket.emit('auth', {
-          channel: { id: channelId },
+          channel: { id: parseInt(channelId) },
           user: { id: user?.id, nickname: user?.nickname },
-        });
+        } as SocketAuthPacket);
       })
       .on('confirm', () => {
         console.log('Connected');
+      })
+      .on('user:join', ({ channel, user }: SocketAuthPacket) => {
+        console.log('an user has joined the room: ', { channel, user });
       })
       .on('message', (message: Message) => {
         setMessages((messages) => [...messages, message]);
@@ -36,11 +68,32 @@ const ChannelScreen: React.FC = () => {
     return () => {
       socket && socket.disconnect();
     };
-  }, [channelId, socket, user?.id]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [channelId]);
+
+  const sendMessage = (message: string) => {
+    socket.emit('message', {
+      user: {
+        id: user?.id,
+        nickname: user?.nickname,
+      },
+      channel: {
+        id: parseInt(channelId),
+      },
+      content: message,
+    } as Message);
+  };
 
   return (
     <div>
-      <p>TYPE MESSAGE TO WIN GAME</p>
+      <div className={classes.messageList}>
+        {messages.map((message) => (
+          <p>{message.content}</p>
+        ))}
+      </div>
+      <Box position="fixed" bottom="0" width="100%">
+        <ChatInput sendMessage={sendMessage} />
+      </Box>
     </div>
   );
 };
