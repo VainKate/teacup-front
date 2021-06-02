3b9261c543e224a31a6510aaac3b9cbfeb3312d0
AngelZatch <angelzatch@gmail.com>
2021-06-02 16:16:54 +0200

    feat: logout

--- src/components/DrawerContent.tsx
+++ src/components/DrawerContent.tsx
@@ -1,9 +1,23 @@
-import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import HomeIcon from '@material-ui/icons/Home';
 import ForumIcon from '@material-ui/icons/Forum';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import { useContext } from 'react';
+import { AuthContext } from '../context/auth';
 
 const DrawerContent: React.FC = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  const onLogout = () => {
+    logout();
+  };
   return (
     <div>
       <List>
@@ -23,6 +37,15 @@ const DrawerContent: React.FC = () => {
             <ListItemText primary={'Découvrir'} />
           </ListItem>
         </Link>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button key={'logout'} onClick={onLogout}>
+          <ListItemIcon>
+            <PowerSettingsNewIcon />
+          </ListItemIcon>
+          <ListItemText primary={'Log out'}></ListItemText>
+        </ListItem>
       </List>
     </div>
   );
