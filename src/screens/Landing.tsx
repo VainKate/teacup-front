e5c57484da03b37ff4859c47d885bab5a5f5bc21
AngelZatch <angelzatch@gmail.com>
2021-06-07 17:11:21 +0200

    feat: stylize landing page a bit

--- src/screens/Landing.tsx
+++ src/screens/Landing.tsx
@@ -1,10 +1,27 @@
-import { Button, Dialog, Typography } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  createStyles,
+  Dialog,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core';
 import { useContext, useState } from 'react';
 import { useHistory } from 'react-router';
 import LoginForm from '../components/LoginForm';
 import { AuthContext } from '../context/auth';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.primary.main,
+    },
+  }),
+);
+
 const LandingScreen: React.FC = () => {
+  const classes = useStyles();
   const { user } = useContext(AuthContext);
   const history = useHistory();
 
@@ -17,28 +34,37 @@ const LandingScreen: React.FC = () => {
   const handleAuthDialogClose = () => setAuthDialogOpen(false);
 
   return (
-    <section>
-      <Typography variant="h1">TeaCup</Typography>
-      <div>
-        <h1>Come chat with us!</h1>
-        <p>
-          Envie de discuter de ta série du moment, de films d'horreur ou de ta
-          passion pour la cuisine ?
-        </p>
-        <p>
-          Rejoins-nous sur les salons de ton choix et viens échanger avec
-          d'autres passionnés !
-        </p>
-      </div>
-      <div>
-        <Button variant="contained" color="inherit" onClick={openAuthDialog}>
-          Login
-        </Button>
-        <Dialog open={isAuthDialogOpen} onClose={handleAuthDialogClose}>
-          <LoginForm />
-        </Dialog>
-      </div>
-    </section>
+    <Box
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      className={classes.root}
+    >
+      <section>
+        <Typography variant="h2">TeaCup</Typography>
+        <div>
+          <h1>Come chat with us!</h1>
+          <p>
+            Envie de discuter de ta série du moment, de films d'horreur ou de ta
+            passion pour la cuisine ?
+          </p>
+          <p>
+            Rejoins-nous sur les salons de ton choix et viens échanger avec
+            d'autres passionnés !
+          </p>
+        </div>
+        <div>
+          <Button variant="contained" color="inherit" onClick={openAuthDialog}>
+            Login
+          </Button>
+          <Dialog open={isAuthDialogOpen} onClose={handleAuthDialogClose}>
+            <LoginForm />
+          </Dialog>
+        </div>
+      </section>
+    </Box>
   );
 };
 
