70cc9d097e3067a6d6666b448fe398e3329ae1f1
Caitlin <caitlin.sigrist@gmail.com>
2021-06-30 16:22:17 +0200

    feat: delete account closes #6

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -4,6 +4,7 @@ import {
   createStyles,
   Dialog,
   makeStyles,
+  TextField,
   Theme,
   Typography,
 } from '@material-ui/core';
@@ -14,8 +15,8 @@ import { Controller, useForm } from 'react-hook-form';
 import NavBar from '../../components/NavBar';
 import { AuthContext } from '../../context/auth';
 import { Tag } from '../../types';
+import DeleteDialog from './DeleteDialog';
 import PasswordForm from './PasswordForm';
-import PersonalData from './PersonalData';
 import TagsContainer from './TagsContainer';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -35,8 +36,17 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: 'space-between',
       minHeight: 'calc(100vh - 64px)',
     },
-    formTitle: {
-      paddingBottom: '0',
+    personalData: {
+      [theme.breakpoints.up('md')]: {
+        margin: '5em 0',
+      },
+    },
+    updatePassword: {
+      marginTop: '2em',
+      backgroundColor: '#0000000a',
+      '&:hover': {
+        backgroundColor: '#0000002b',
+      },
     },
     buttonsContainer: {
       display: 'flex',
@@ -112,16 +122,81 @@ const ProfileScreen: React.FC = () => {
   const openPasswordDialog = () => setPasswordDialogOpen(true);
   const handlePasswordDialogClose = () => setPasswordDialogOpen(false);
 
+  // Delete dialog
+  const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const openDeleteDialog = () => setDeleteDialogOpen(true);
+  const handleDeleteDialogClose = () => setDeleteDialogOpen(false);
+
   return (
     <>
       <NavBar />
       <Box className={classes.root}>
         <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
           <Box>
-            <PersonalData
-              openPasswordDialog={openPasswordDialog}
-              control={control}
-            />
+            <Box className={classes.personalData}>
+              <Typography variant="h4">Tes infos personnelles</Typography>
+              <Box display="flex" flexDirection="column" margin="3em 0">
+                <Controller
+                  control={control}
+                  name="email"
+                  rules={{
+                    required: "L'adresse mail est obligatoire.",
+                    validate: (value) =>
+                      value && value?.replaceAll(' ', '').length !== 0,
+                  }}
+                  render={({
+                    field: { onChange, onBlur, value, ref },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      onBlur={onBlur}
+                      onChange={onChange}
+                      inputRef={ref}
+                      value={value}
+                      label="Adresse mail"
+                      margin="dense"
+                      id="email"
+                      type="email"
+                      error={!!error}
+                      helperText={error?.message}
+                    />
+                  )}
+                />
+                <Controller
+                  control={control}
+                  name="nickname"
+                  rules={{
+                    required: 'Le pseudo est obligatoire.',
+                    validate: (value) =>
+                      value && value?.replaceAll(' ', '').length !== 0,
+                  }}
+                  render={({
+                    field: { onChange, onBlur, value, ref },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      onBlur={onBlur}
+                      onChange={onChange}
+                      inputRef={ref}
+                      value={value}
+                      label="Pseudo"
+                      margin="dense"
+                      id="nickname"
+                      error={!!error}
+                      helperText={error?.message}
+                    />
+                  )}
+                />
+                <Button
+                  size="large"
+                  type="button"
+                  className={classes.updatePassword}
+                  onClick={openPasswordDialog}
+                >
+                  Modifier ton mot de passe
+                </Button>
+              </Box>
+            </Box>
             <Box>
               <Typography variant="h4">Tes centres d'intérêts</Typography>
               <Controller
@@ -144,6 +219,7 @@ const ProfileScreen: React.FC = () => {
               size="large"
               type="button"
               startIcon={<DeleteIcon />}
+              onClick={openDeleteDialog}
             >
               Supprimer le compte
             </Button>
@@ -167,6 +243,16 @@ const ProfileScreen: React.FC = () => {
           onClose={handlePasswordDialogClose}
         >
           <PasswordForm handlePasswordDialogClose={handlePasswordDialogClose} />
+        </Dialog>
+      )}
+      {isDeleteDialogOpen && (
+        <Dialog
+          fullWidth
+          maxWidth="sm"
+          open={isDeleteDialogOpen}
+          onClose={handleDeleteDialogClose}
+        >
+          <DeleteDialog handleDeleteDialogClose={handleDeleteDialogClose} />
         </Dialog>
       )}
     </>
